b1b6e328fb0910d6cddc852c53bf119a6ee6ab32
mzijta <marcellazijta@gmail.com>
2024-01-18 14:05:08 +0100

    changed autoencoder to have the same output vector size as the teacher

--- common3d.py
+++ common3d.py
@@ -3,6 +3,7 @@
 from torch import nn
 from torchvision.datasets import ImageFolder
 
+
 def get_autoencoder(out_channels=384):
     return nn.Sequential(
         # encoder
@@ -15,51 +16,36 @@ def get_autoencoder(out_channels=384):
         nn.Conv3d(in_channels=32, out_channels=64, kernel_size=4, stride=2,
                   padding=1),
         nn.ReLU(inplace=True),
-        nn.Conv3d(in_channels=64, out_channels=64, kernel_size=4, stride=2,
-                  padding=1),
-        nn.ReLU(inplace=True),
-        nn.Conv3d(in_channels=64, out_channels=64, kernel_size=4, stride=2,
-                  padding=1),
-        nn.ReLU(inplace=True),
         nn.Conv3d(in_channels=64, out_channels=64, kernel_size=8),
         # decoder
-        nn.Upsample(size=3, mode='bilinear'),
+        nn.Upsample(size=3, mode='trilinear'),
         nn.Conv3d(in_channels=64, out_channels=64, kernel_size=4, stride=1,
                   padding=2),
         nn.ReLU(inplace=True),
         nn.Dropout(0.2),
-        nn.Upsample(size=8, mode='bilinear'),
+        nn.Upsample(size=8, mode='trilinear'),
         nn.Conv3d(in_channels=64, out_channels=64, kernel_size=4, stride=1,
                   padding=2),
         nn.ReLU(inplace=True),
         nn.Dropout(0.2),
-        nn.Upsample(size=15, mode='bilinear'),
+        nn.Upsample(size=15, mode='trilinear'),
         nn.Conv3d(in_channels=64, out_channels=64, kernel_size=4, stride=1,
                   padding=2),
         nn.ReLU(inplace=True),
         nn.Dropout(0.2),
-        nn.Upsample(size=32, mode='bilinear'),
+        nn.Upsample(size=32, mode='trilinear'),
         nn.Conv3d(in_channels=64, out_channels=64, kernel_size=4, stride=1,
                   padding=2),
         nn.ReLU(inplace=True),
         nn.Dropout(0.2),
-        nn.Upsample(size=63, mode='bilinear'),
-        nn.Conv3d(in_channels=64, out_channels=64, kernel_size=4, stride=1,
-                  padding=2),
-        nn.ReLU(inplace=True),
-        nn.Dropout(0.2),
-        nn.Upsample(size=127, mode='bilinear'),
-        nn.Conv3d(in_channels=64, out_channels=64, kernel_size=4, stride=1,
-                  padding=2),
-        nn.ReLU(inplace=True),
-        nn.Dropout(0.2),
-        nn.Upsample(size=56, mode='bilinear'),
+        nn.Upsample(size=8, mode='trilinear'),
         nn.Conv3d(in_channels=64, out_channels=64, kernel_size=3, stride=1,
                   padding=1),
         nn.ReLU(inplace=True),
         nn.Conv3d(in_channels=64, out_channels=out_channels, kernel_size=3,
                   stride=1, padding=1)
     )
+
 
 def get_pdn_xsmall(out_channels=384, padding=False):
     pad_mult = 1 if padding else 0
@@ -77,7 +63,8 @@ def get_pdn_xsmall(out_channels=384, padding=False):
         nn.ReLU(inplace=True),
         nn.Conv3d(in_channels=64, out_channels=out_channels, kernel_size=4)
     )
-    
+
+
 def get_pdn_small(out_channels=384, padding=False):
     pad_mult = 1 if padding else 0
     return nn.Sequential(
@@ -94,6 +81,7 @@ def get_pdn_small(out_channels=384, padding=False):
         nn.ReLU(inplace=True),
         nn.Conv3d(in_channels=256, out_channels=out_channels, kernel_size=4)
     )
+
 
 def get_pdn_medium(out_channels=384, padding=False):
     pad_mult = 1 if padding else 0
@@ -117,16 +105,19 @@ def get_pdn_medium(out_channels=384, padding=False):
                   kernel_size=1)
     )
 
+
 class ImageFolderWithoutTarget(ImageFolder):
     def __getitem__(self, index):
         sample, target = super().__getitem__(index)
         return sample
+
 
 class ImageFolderWithPath(ImageFolder):
     def __getitem__(self, index):
         path, target = self.samples[index]
         sample, target = super().__getitem__(index)
         return sample, target, path
+
 
 def InfiniteDataloader(loader):
     iterator = iter(loader)
