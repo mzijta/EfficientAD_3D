b2c8681e483b9d6841c7dad36281357478d93eee
mzijta <marcellazijta@gmail.com>
2024-01-18 14:56:56 +0100

    rewrote for 3d. Try out on the gpu cluster

--- efficientad_3d.py
+++ efficientad_3d.py
@@ -8,6 +8,7 @@ from torchvision import transforms
 import argparse
 import itertools
 import os
+import nibabel as nib
 import random
 from tqdm import tqdm
 from common3d import get_autoencoder, get_pdn_small, get_pdn_medium, \
@@ -18,13 +19,13 @@ def get_argparse():
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--dataset', default='mvtec_ad',
                         choices=['mvtec_ad', 'mvtec_loco'])
-    parser.add_argument('-s', '--subdataset', default='embryos',
+    parser.add_argument('-s', '--subdataset', default='embryos_small',
                         help='One of 15 sub-datasets of Mvtec AD or 5' +
                              'sub-datasets of Mvtec LOCO')
     parser.add_argument('-o', '--output_dir', default='output_3d_model')
     parser.add_argument('-m', '--model_size', default='small',
                         choices=['small', 'medium'])
-    parser.add_argument('-w', '--weights', default='models/teacher_small.pth')
+    parser.add_argument('-w', '--weights', default='models/teacher_small_final_state.pth')
     parser.add_argument('-i', '--imagenet_train_path',
                         default='none',
                         help='Set to "none" to disable ImageNet' +
@@ -36,7 +37,7 @@ def get_argparse():
     parser.add_argument('-b', '--mvtec_loco_path',
                         default='./mvtec_loco_anomaly_detection',
                         help='Downloaded Mvtec LOCO dataset')
-    parser.add_argument('-t', '--train_steps', type=int, default=70000)
+    parser.add_argument('-t', '--train_steps', type=int, default=7)
     return parser.parse_args()
 
 # constants
@@ -56,6 +57,11 @@ transform_ae = transforms.RandomChoice([
     transforms.ColorJitter(contrast=0.2),
     transforms.ColorJitter(saturation=0.2)
 ])
+
+
+def list_full_paths(directory):
+    return [os.path.join(directory, file) for file in os.listdir(directory)]
+
 
 def train_transform(image):
     return default_transform(image), default_transform(transform_ae(image))
@@ -83,37 +89,24 @@ def main():
                                     config.dataset, config.subdataset)
     test_output_dir = os.path.join(config.output_dir, 'anomaly_maps',
                                    config.dataset, config.subdataset, 'test')
-    os.makedirs(train_output_dir)
-    os.makedirs(test_output_dir)
+    if not os.path.isdir(train_output_dir):
+        os.makedirs(train_output_dir)
+    if not os.path.isdir(test_output_dir):
+        os.makedirs(test_output_dir)
 
     # load data
-    full_train_set = ImageFolderWithoutTarget(
-        os.path.join(dataset_path, config.subdataset, 'train'),
-        transform=transforms.Lambda(train_transform))
-    test_set = ImageFolderWithPath(
-        os.path.join(dataset_path, config.subdataset, 'test'))
-    if config.dataset == 'mvtec_ad':
-        # mvtec dataset paper recommend 10% validation set
-        train_size = int(0.9 * len(full_train_set))
-        validation_size = len(full_train_set) - train_size
-        rng = torch.Generator().manual_seed(seed)
-        train_set, validation_set = torch.utils.data.random_split(full_train_set,
-                                                           [train_size,
-                                                            validation_size],
-                                                           rng)
-    elif config.dataset == 'mvtec_loco':
-        train_set = full_train_set
-        validation_set = ImageFolderWithoutTarget(
-            os.path.join(dataset_path, config.subdataset, 'validation'),
-            transform=transforms.Lambda(train_transform))
-    else:
-        raise Exception('Unknown config.dataset')
+    full_train_set = list_full_paths(os.path.join(dataset_path, config.subdataset, 'train', 'good'))
+    test_set_path = os.path.join(dataset_path, config.subdataset, 'test')
+    test_set_bad = list_full_paths(os.path.join(test_set_path,'bad'))
+    test_set_good = list_full_paths(os.path.join(test_set_path, 'good'))
+    validation_set = list_full_paths(os.path.join(dataset_path, config.subdataset, 'val', 'good'))
+    train_set = full_train_set
+    print(train_set)
 
-
-    train_loader = DataLoader(train_set, batch_size=1, shuffle=True,
-                              num_workers=4, pin_memory=True)
-    train_loader_infinite = InfiniteDataloader(train_loader)
-    validation_loader = DataLoader(validation_set, batch_size=1)
+    #train_loader = DataLoader(train_set, batch_size=1, shuffle=True,
+    #                          num_workers=4, pin_memory=True)
+    #train_loader_infinite = InfiniteDataloader(train_loader)
+    #validation_loader = DataLoader(validation_set, batch_size=1)
 
     if pretrain_penalty:
         # load pretraining data for penalty
@@ -156,16 +149,32 @@ def main():
         student.cuda()
         autoencoder.cuda()
 
-    teacher_mean, teacher_std = teacher_normalization(teacher, train_loader)
+    teacher_mean, teacher_std = teacher_normalization(teacher, train_set)
 
     optimizer = torch.optim.Adam(itertools.chain(student.parameters(),
                                                  autoencoder.parameters()),
                                  lr=1e-4, weight_decay=1e-5)
     scheduler = torch.optim.lr_scheduler.StepLR(
         optimizer, step_size=int(0.95 * config.train_steps), gamma=0.1)
+
+    q_st_start, q_st_end, q_ae_start, q_ae_end = map_normalization(
+        validation_loader=validation_set, teacher=teacher, student=student,
+        autoencoder=autoencoder, teacher_mean=teacher_mean,
+        teacher_std=teacher_std, desc='Final map normalization')
+    auc = test(
+        test_set_good=test_set_good, test_set_defect=test_set_bad, teacher=teacher, student=student,
+        autoencoder=autoencoder, teacher_mean=teacher_mean,
+        teacher_std=teacher_std, q_st_start=q_st_start, q_st_end=q_st_end,
+        q_ae_start=q_ae_start, q_ae_end=q_ae_end,
+        test_output_dir=test_output_dir, desc='Final inference')
+
     tqdm_obj = tqdm(range(config.train_steps))
-    for iteration, (image_st, image_ae), image_penalty in zip(
-            tqdm_obj, train_loader_infinite, penalty_loader_infinite):
+    for iteration,  image_penalty in zip(
+            tqdm_obj, penalty_loader_infinite):
+        index_train_list = iteration % len(train_set)
+        image_in_list = train_set[index_train_list]
+        image_st = load_nifti_image_to_5d_tensor(image_in_list, image_size)
+        image_ae = load_nifti_image_to_5d_tensor(image_in_list, image_size)
         if on_gpu:
             image_st = image_st.cuda()
             image_ae = image_ae.cuda()
@@ -221,12 +230,12 @@ def main():
             autoencoder.eval()
 
             q_st_start, q_st_end, q_ae_start, q_ae_end = map_normalization(
-                validation_loader=validation_loader, teacher=teacher,
+                validation_loader=validation_set, teacher=teacher,
                 student=student, autoencoder=autoencoder,
                 teacher_mean=teacher_mean, teacher_std=teacher_std,
                 desc='Intermediate map normalization')
             auc = test(
-                test_set=test_set, teacher=teacher, student=student,
+                test_set_good=test_set_good, test_set_defect=test_set_bad, teacher=teacher, student=student,
                 autoencoder=autoencoder, teacher_mean=teacher_mean,
                 teacher_std=teacher_std, q_st_start=q_st_start,
                 q_st_end=q_st_end, q_ae_start=q_ae_start, q_ae_end=q_ae_end,
@@ -248,27 +257,34 @@ def main():
                                          'autoencoder_final.pth'))
 
     q_st_start, q_st_end, q_ae_start, q_ae_end = map_normalization(
-        validation_loader=validation_loader, teacher=teacher, student=student,
+        validation_loader=validation_set, teacher=teacher, student=student,
         autoencoder=autoencoder, teacher_mean=teacher_mean,
         teacher_std=teacher_std, desc='Final map normalization')
     auc = test(
-        test_set=test_set, teacher=teacher, student=student,
+        test_set_good=test_set_good, test_set_defect=test_set_bad, teacher=teacher, student=student,
         autoencoder=autoencoder, teacher_mean=teacher_mean,
         teacher_std=teacher_std, q_st_start=q_st_start, q_st_end=q_st_end,
         q_ae_start=q_ae_start, q_ae_end=q_ae_end,
         test_output_dir=test_output_dir, desc='Final inference')
     print('Final image auc: {:.4f}'.format(auc))
 
-def test(test_set, teacher, student, autoencoder, teacher_mean, teacher_std,
+def test(test_set_good, test_set_defect, teacher, student, autoencoder, teacher_mean, teacher_std,
          q_st_start, q_st_end, q_ae_start, q_ae_end, test_output_dir=None,
          desc='Running inference'):
     y_true = []
     y_score = []
-    for image, target, path in tqdm(test_set, desc=desc):
-        orig_width = image.width
-        orig_height = image.height
-        image = default_transform(image)
-        image = image[None]
+    list_good_defect = len(test_set_good)*['good'] + len(test_set_defect)*['defect']
+    test_set = test_set_good + test_set_defect
+    i=0
+    for image_in_list in test_set:
+        image = load_nifti_image_to_5d_tensor(image_in_list, image_size)
+        og_image = nib.load(image_in_list)
+        og_image_array = og_image.get_fdata()
+        shape_og_image = np.shape(og_image_array)
+        orig_width = shape_og_image[0]
+        orig_height = shape_og_image[1]
+        orig_length = shape_og_image[2]
+        #image = image[None]
         if on_gpu:
             image = image.cuda()
         map_combined, map_st, map_ae = predict(
@@ -276,23 +292,29 @@ def test(test_set, teacher, student, autoencoder, teacher_mean, teacher_std,
             autoencoder=autoencoder, teacher_mean=teacher_mean,
             teacher_std=teacher_std, q_st_start=q_st_start, q_st_end=q_st_end,
             q_ae_start=q_ae_start, q_ae_end=q_ae_end)
-        map_combined = torch.nn.functional.pad(map_combined, (4, 4, 4, 4))
+        map_combined = torch.nn.functional.pad(map_combined, (4, 4, 4, 4, 4, 4))
+        print(map_combined.size())
+        print(map_combined)
         map_combined = torch.nn.functional.interpolate(
-            map_combined, (orig_height, orig_width), mode='bilinear')
+            map_combined, (orig_height, orig_width, orig_length), mode='trilinear')
         map_combined = map_combined[0, 0].cpu().numpy()
 
-        defect_class = os.path.basename(os.path.dirname(path))
+        defect_class = list_good_defect[i]
         if test_output_dir is not None:
-            img_nm = os.path.split(path)[1].split('.')[0]
+            img_nm = os.path.basename(image_in_list)
             if not os.path.exists(os.path.join(test_output_dir, defect_class)):
                 os.makedirs(os.path.join(test_output_dir, defect_class))
-            file = os.path.join(test_output_dir, defect_class, img_nm + '.tiff')
-            tifffile.imwrite(file, map_combined)
+            file = os.path.join(test_output_dir, defect_class, img_nm)
+            final_img = nib.Nifti1Image(map_combined, og_image.affine)
+            nib.save(final_img, file)
 
         y_true_image = 0 if defect_class == 'good' else 1
         y_score_image = np.max(map_combined)
+        print(y_true_image)
+        print(y_score_image)
         y_true.append(y_true_image)
         y_score.append(y_score_image)
+        i = i + 1
     auc = roc_auc_score(y_true=y_true, y_score=y_score)
     return auc * 100
 
@@ -321,7 +343,8 @@ def map_normalization(validation_loader, teacher, student, autoencoder,
     maps_st = []
     maps_ae = []
     # ignore augmented ae image
-    for image, _ in tqdm(validation_loader, desc=desc):
+    for image_in_list in validation_loader:
+        image = load_nifti_image_to_5d_tensor(image_in_list,image_size)
         if on_gpu:
             image = image.cuda()
         map_combined, map_st, map_ae = predict(
@@ -342,28 +365,38 @@ def map_normalization(validation_loader, teacher, student, autoencoder,
 def teacher_normalization(teacher, train_loader):
 
     mean_outputs = []
-    for train_image, _ in tqdm(train_loader, desc='Computing mean of features'):
+    for train_image_in_list in train_loader:
+        print(train_image_in_list)
+        train_image = load_nifti_image_to_5d_tensor(train_image_in_list, image_size)
         if on_gpu:
             train_image = train_image.cuda()
         teacher_output = teacher(train_image)
-        mean_output = torch.mean(teacher_output, dim=[0, 2, 3])
+        mean_output = torch.mean(teacher_output, dim=[0, 2, 3,4])
         mean_outputs.append(mean_output)
     channel_mean = torch.mean(torch.stack(mean_outputs), dim=0)
-    channel_mean = channel_mean[None, :, None, None]
+    channel_mean = channel_mean[None, :, None, None, None]
 
     mean_distances = []
-    for train_image, _ in tqdm(train_loader, desc='Computing std of features'):
+    for train_image_in_list in train_loader:
+        train_image = load_nifti_image_to_5d_tensor(train_image_in_list, image_size)
         if on_gpu:
             train_image = train_image.cuda()
         teacher_output = teacher(train_image)
         distance = (teacher_output - channel_mean) ** 2
-        mean_distance = torch.mean(distance, dim=[0, 2, 3])
+        mean_distance = torch.mean(distance, dim=[0, 2, 3,4])
         mean_distances.append(mean_distance)
     channel_var = torch.mean(torch.stack(mean_distances), dim=0)
-    channel_var = channel_var[None, :, None, None]
+    channel_var = channel_var[None, :, None, None, None]
     channel_std = torch.sqrt(channel_var)
 
     return channel_mean, channel_std
+
+def load_nifti_image_to_5d_tensor(image_in_list, image_size):
+    image = nib.load(image_in_list).get_fdata()
+    image = np.resize(image, (image_size, image_size, image_size))
+    image = np.resize(image, (1, 3, image_size, image_size, image_size)).astype(np.float32)
+    image = torch.from_numpy(image)
+    return image
 
 if __name__ == '__main__':
     main()
